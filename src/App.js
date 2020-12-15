bf46cc7eec62adb251add75fe04854c0a9ede152
arenignacio <business.arenignacio@gmail.com>
2020-12-15 15:31:35 -0500

    Add section divisions

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Nav from './components/Nav';
 import Content from './components/Content';
-
 import './components/Nav.scss';
+import avatar from './img/avatar.jpg';
 
 /* 
 TODO:
@@ -39,17 +39,85 @@ export default () => {
 	return (
 		<div>
 			<Nav options={navLinks} />
+			<div className="header">
+				<div className="hero-image">
+					<div className="greeting-container hero-text">
+						<p>Hello! I am Aren.</p>
+						<p>I make web applications.</p>
+						<hr className="bar"></hr>
+						<button type="button">Hire Me</button>
+
+						<a href="#about">
+							<div className="arrow down exit arrow-spread"></div>
+							<div className="arrow down exit-2 arrow-spread"></div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="about" id="about">
+				<div className="general-container">
+					<div className="row">
+						<div className="col-8 aboutme-container">
+							<h1>About Me</h1>
+							<p>
+								My name is Aren Ignacio and I create web applications
+								using modern tools.
+							</p>
+						</div>
+						<img className="col-4 headshot" src={avatar} alt="headshot" />
+					</div>
+					<row>
+						<div className="skills-container">
+							<h1>Technical Skills</h1>
+							<div className="card-row">
+								<div className="card"></div>
+								<div className="card"></div>
+								<div className="card"></div>
+								<div className="card"></div>
+							</div>
+						</div>
+					</row>
+				</div>
+			</div>
+			<div className="projects-contact">
+				<div className="projects-container">
+					<card></card>
+					<card></card>
+					<card></card>
+				</div>
+				<hr></hr>
+				<row>
+					<div className="contact-container">
+						<h1>Contact Info</h1>
+						<p>contact info</p>
+						<p>contact info</p>
+						<p>contact info</p>
+					</div>
+					<div className="form-container">
+						<form action="">
+							<field></field>
+							<field></field>
+							<field></field>
+						</form>
+					</div>
+				</row>
+			</div>
+			{/* 
 			<div className="layer-0">
 				<div className="greetings-container layer-0">
 					<div className="hero-image"></div>
 					<div className="hero-text">
 						<div>
-							<span style={{ fontSize: '60px' }}>H</span>
-							ello, I&apos;m
-							<span> A</span>
-							ren.
+							<p>
+								<span style={{ fontSize: '60px' }}>H</span>
+								ello, I&apos;m
+								<span> A</span>
+								ren.
+							</p>
 						</div>
-						<div>I make Web Applications.</div>
+						<div>
+							<p>I make Web Applications.</p>
+						</div>
 						<button className="" type="button">
 							Hire me
 						</button>
@@ -60,7 +128,7 @@ export default () => {
 				</div>
 				<div className=" border Projects px-sm-2">Projects</div>
 				<div className="border Contact px-sm-2">Contact</div>
-			</div>
+			</div> */}
 		</div>
 	);
 };
